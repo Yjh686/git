4aa1abdd554adfea8491cef3bcfb7c5107f5798a
应家宏 <2534715493@qq.com>
2026-07-14 10:06:10 +0800

    优化了登录功能并加入了验证码模块

--- src/ui/login.java
+++ src/ui/login.java
@@ -4,6 +4,7 @@ import javabean.User;
 
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class login {
@@ -30,35 +31,57 @@ public class login {
 
     }
     //登录操作
-    public void login(){
+    public void login() {
         System.out.println("用户选择了登录");
-        int cishu=3;
-        Scanner sc=new Scanner(System.in);
-        while (true) {
-            System.out.println("请输入用户名：");
-            String username=sc.next();
-            System.out.println("请输入密码：");
-            String password=sc.next();
-            for (int i = 0; i < list.size(); i++) {
-                User u=list.get(i);
-                if(!(u.getUsername().equals(username)&&u.getPassword().equals(password))){
-                    System.out.println("用户名或密码错误,请重新输入");
-                    cishu--;
-                }
-                else{
-                    System.out.println("登录成功");
-                    return;
-                }
-
-                }
-            if(cishu==0){
-                System.out.println("输入错误次数过多，退出登录");
-                break;
-
-            }
-
+        //判断用户名 获取用户对象
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入用户名：");
+        String username = sc.next();
+        if (!contains(list, username)) {
+            System.out.println("用户名不存在,请注册");
+            return;
         }
+        int index = findindex(list, username);
+        //判断状态
+        if (list.get(index).isStatus() == false) {
+            System.out.println("用户已禁用,请联系YJH");
+            return;
+        }
+        //输入验证码
 
+
+        //输入密码
+        for (int i = 0; i < 3; i++) {
+            System.out.println("请输入密码：");
+            String password = sc.next();
+            if(!password.equals(list.get(index).getPassword())){
+                System.out.println("密码错误,请重新输入");
+            }
+            while (true) {
+                System.out.println("验证码为：");
+                String yanzhengma = yanzhengma();
+                System.out.println(yanzhengma);
+                System.out.print("请输入验证码：");
+                String code = sc.next();
+                if (!code.equalsIgnoreCase(yanzhengma)) {
+                    System.out.println("验证码错误,请重新输入");
+                    continue;
+                }
+                else {
+                    System.out.println("验证码正确");
+                    break;
+                }
+            }
+            if(i==2){
+                System.out.println("密码错误次数过多,账户已锁定");
+                list.get(index).setStatus(false);
+                return;
+            }
+            if(password.equals(list.get(index).getPassword())){
+                System.out.println("登录成功");
+                return;
+            }
+        }
     }
     //注册操作
     public void register(){
@@ -128,7 +151,7 @@ public class login {
         return count[0]>=0&&count[1]>0&&count[2]==0;
     }
 
-    public boolean contains(ArrayList<User> list,String username){
+    public  boolean contains(ArrayList<User> list,String username){
         for (int i = 0; i < list.size(); i++) {
             User u=list.get(i);
             if(u.getUsername().equals(username)){
@@ -159,6 +182,36 @@ public class login {
         return new int[]{numcount,charcount,othercount};
     }
 
+    public static String yanzhengma() {
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            list.add((char) ('a' + i));
+            list.add((char) ('A' + i));
+        }
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            sb.append(list.get(r.nextInt(list.size())));
+        }
+        sb.append(r.nextInt(10));
+        char[] arr = sb.toString().toCharArray();
+        int rr=r.nextInt(arr.length);
+        char temp = arr[4];
+        arr[4] = arr[rr];
+        arr[rr]=temp;
+
+        return new String(arr);
+    }
+
+    public int findindex(ArrayList<User> list,String username){
+        for (int i = 0; i < list.size(); i++) {
+            User u=list.get(i);
+            if(u.getUsername().equals(username)){
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
 
